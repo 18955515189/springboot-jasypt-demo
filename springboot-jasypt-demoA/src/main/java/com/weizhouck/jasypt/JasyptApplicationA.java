f2382e73de2e160dd21b264b7cd595670449d24c
18955515189 <18955515189@163.com>
2020-06-20 01:11:59 +0800

    springboot-jasypt-demo

---
+++ springboot-jasypt-demoA/src/main/java/com/weizhouck/jasypt/JasyptApplicationA.java
@@ -0,0 +1,19 @@
+package com.weizhouck.jasypt;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 飞翔的胖哥
+ * @version 1.0.0
+ * @description 启动引导类 重点：添加 @EnableEncryptableProperties
+ * @since 2020/6/20 0020 0:56
+ **/
+@SpringBootApplication
+@EnableEncryptableProperties
+public class JasyptApplicationA {
+    public static void main(String[] args){
+        SpringApplication.run(JasyptApplicationA.class,args);
+    }
+}
---
+++ springboot-jasypt-demoA/src/main/java/com/weizhouck/jasypt/controller/MineController.java
@@ -0,0 +1,24 @@
+package com.weizhouck.jasypt.controller;
+
+import com.weizhouck.jasypt.config.MineProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 飞翔的胖哥
+ * @version 1.0.0
+ * @description MineController
+ * @since 2020/6/20 0020 1:03
+ **/
+@RestController
+public class MineController {
+
+    @Autowired
+    private MineProperties mineProperties;
+
+    @GetMapping("get")
+    public String get(){
+        return mineProperties.toString();
+    }
+}

76fc73c4b97f7de2b2e2876b2f42c39e0fc383bd
18955515189 <18955515189@163.com>
2020-06-20 01:16:25 +0800

    2

--- springboot-jasypt-demoA/src/main/java/com/weizhouck/jasypt/JasyptApplicationA.java
+++ springboot-jasypt-demoA/src/main/java/com/weizhouck/jasypt/JasyptApplicationA.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author 飞翔的胖哥
  * @version 1.0.0
- * @description 启动引导类 重点：添加 @EnableEncryptableProperties
+ * @description 启动引导类 重点：添 加 @EnableEncryptableProperties
  * @since 2020/6/20 0020 0:56
  **/
 @SpringBootApplication
